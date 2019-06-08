8b80568bd4e69fe0c64337b3ce8b31aa416034df
James Callahan <jamescallahan@uchicago.edu>
2019-06-08 14:36:16 -0400

    1D functionality test

--- test_lattice.py
+++ test_lattice.py
@@ -6,6 +6,9 @@ def test_lattice_number_neigbors():
   lattice = Lattice(3,3)
 
   for place in [(0,0),(0,1),(1,1)]:
-    answer = lattice.find_neighbors((1,1))
+    answer = lattice.find_neighbors(place)
     assert len(answer)==4, "Wrong number of neighbors"
+  
+  lattice2 = Lattice(5,1)
+  assert len( lattice2.find_neighbors((0,0)) )==2, "1D functionality broken"
 
