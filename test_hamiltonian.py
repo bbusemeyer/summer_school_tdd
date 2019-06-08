ae6ee25684af7524e5a613aa4286fecf1481abf7
Brian Busemeyer <bbusemeyer@gmail.com>
2019-06-08 14:32:28 -0500

    AFM test

--- test_hamiltonian.py
+++ test_hamiltonian.py
@@ -1,4 +1,4 @@
-from numpy import ones
+from numpy import ones,tile,eye
 from lattice import Lattice
 from hamiltonian import Hamiltonian
 
@@ -12,3 +12,16 @@ def test_energy_ferro():
   ham = Hamiltonian(lattice,coupling)
 
   assert ham.energy(ones((size,size),dtype=bool)) == 2*coupling*size*size, "Energy of ferromagnetic state is incorrect."
+
+def test_energy_ferro():
+  ''' Check the energy of the ferromagnetic state.'''
+
+  size = 10
+  coupling = 1.0
+
+  lattice = Lattice(size,size)
+  ham = Hamiltonian(lattice,coupling)
+  
+  afm = tile(eye(2,dtype=bool),(5,5))
+
+  assert ham.energy(ones((size,size),dtype=bool)) == -2*coupling*size*size, "Energy of antiferromagnetic state is incorrect."
